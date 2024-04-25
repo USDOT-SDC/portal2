d39892a146758ff6c18770175b152943a3fa8f6f
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2024-04-24 22:18:53 -0400

    update the outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,29 @@
 # use caution when making changes to local.common
 # local.common is output to tfstate and can be used by other configurations
-output "Repository" {
-  value = local.default_tags.Repository
+output "debug" {
+  value = {
+    environment = local.common.environment
+    repository  = local.default_tags.Repository
+  }
+}
+
+output "backend" {
+  value = {
+    rest_api = {
+      id  = module.be.rest_api.id
+      url = module.be.rest_api.url
+    }
+    s3 = {
+      portal_bucket = module.be.s3.portal_bucket
+    }
+  }
+}
+
+output "frontend" {
+  value = {
+    s3 = {
+      portal_bucket    = module.be.s3.portal_bucket
+      objects_deployed = module.fe.s3.objects_deployed
+    }
+  }
 }
