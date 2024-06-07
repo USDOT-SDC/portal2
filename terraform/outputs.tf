a09d48d2a967549db9867c6ea5cc5d255b94609b
Jeff Ussing <Jeff.Ussing.CTR@dot.gov>
2024-06-06 23:00:46 -0400

    cleanup input and output vars

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,20 +1,24 @@
-# use caution when making changes to local.common
-# local.common is output to tfstate and can be used by other configurations
-output "debug" {
-  value = {
-    environment = local.common.environment
-    repository  = local.default_tags.Repository
-  }
-}
+# use caution when making changes to outputs
+# they are put into the tfstate file and can be used by other Terraform configurations
+# output "debug" {
+#   value = {
+#     infrastructure_outputs = data.terraform_remote_state.infrastructure.outputs
+#   }
+# }
 
 output "backend" {
   value = {
     resource_urls = module.be.resource_urls
     s3 = {
-      portal_bucket = module.be.s3.portal_bucket
+      portal = module.be.s3.portal
     }
     route53_zone = {
-      public = module.be.route53_zone_public
+      public = {
+        arn     = module.be.route53_zone_public.arn
+        id      = module.be.route53_zone_public.id
+        name    = module.be.route53_zone_public.name
+        zone_id = module.be.route53_zone_public.zone_id
+      }
     }
   }
 }
@@ -22,8 +26,7 @@ output "backend" {
 output "frontend" {
   value = {
     s3 = {
-      portal_bucket    = module.be.s3.portal_bucket
-      objects_deployed = module.fe.s3.objects_deployed
+      deployed_objects = module.fe.s3.deployed_objects
     }
   }
 }
