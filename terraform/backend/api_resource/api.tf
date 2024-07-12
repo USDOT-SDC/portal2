1ee77d4ffabad3630e0752f2a9e743c4ccfc6c0c
Brandon Fitzpatrick <b.fitzpatrick.ctr@dot.gov>
2024-07-12 09:54:55 -0400

    add options method to all api resources

--- terraform/backend/api_resource/api.tf
+++ terraform/backend/api_resource/api.tf
@@ -16,6 +16,13 @@ resource "aws_api_gateway_method" "m" {
   authorizer_id = var.authorizer_id
 }
 
+resource "aws_api_gateway_method" "om" {
+  rest_api_id   = var.rest_api.id
+  resource_id   = aws_api_gateway_resource.r.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
 # === Resource -> Integration ===
 resource "aws_api_gateway_integration" "i" {
   rest_api_id             = var.rest_api.id
@@ -28,13 +35,16 @@ resource "aws_api_gateway_integration" "i" {
 
 }
 
-# === Resource -> Integration -> Response ===
-# resource "aws_api_gateway_integration_response" "ir" {
-#   rest_api_id = var.rest_api.id
-#   resource_id = aws_api_gateway_resource.r.id
-#   http_method = aws_api_gateway_method.m.http_method
-#   status_code = aws_api_gateway_method_response.mr.status_code
-# }
+resource "aws_api_gateway_integration" "oi" {
+  rest_api_id             = var.rest_api.id
+  resource_id             = aws_api_gateway_resource.r.id
+  http_method             = aws_api_gateway_method.om.http_method
+  type                    = "MOCK"
+  request_templates        = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+  depends_on = [ aws_api_gateway_method.om ]
+}
 
 # === Resource -> Method -> Response ===
 resource "aws_api_gateway_method_response" "mr" {
@@ -45,4 +55,40 @@ resource "aws_api_gateway_method_response" "mr" {
   response_models = {
     "application/json" = "Empty"
   }
+}
+
+resource "aws_api_gateway_method_response" "omr" {
+  rest_api_id = var.rest_api.id
+  resource_id = aws_api_gateway_resource.r.id
+  http_method = aws_api_gateway_method.om.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Credentials" = true,
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true,
+    "method.response.header.Access-Control-Expose-Headers" = true,
+    "method.response.header.Access-Control-Max-Age" = true
+  }
+  response_models = {
+    "application/json" = "Empty"
+  }
+  depends_on = [ aws_api_gateway_method.om ]
+}
+
+# === Resource -> Integration -> Response ===
+resource "aws_api_gateway_integration_response" "oir" {
+  rest_api_id = var.rest_api.id
+  resource_id = aws_api_gateway_resource.r.id
+  http_method = aws_api_gateway_method.om.http_method
+  status_code = aws_api_gateway_method_response.omr.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Credentials" = "'true'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'",
+    "method.response.header.Access-Control-Expose-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Access-Control-Allow-Origin'",
+    "method.response.header.Access-Control-Max-Age" = "'600'"
+  }
+  depends_on = [ aws_api_gateway_method_response.omr ]
 }
