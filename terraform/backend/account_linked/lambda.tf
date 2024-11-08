2758f747b8dd39f56d931b7825a675d836296dd3
Fitzpatrick <b.fitzpatrick.ctr@ad.dot.gov>
2024-11-08 07:50:28 -0500

    update environment variables

--- terraform/backend/account_linked/lambda.tf
+++ terraform/backend/account_linked/lambda.tf
@@ -6,7 +6,7 @@ data "archive_file" "account_linked" {
 
 resource "aws_lambda_function" "account_linked" {
   function_name    = "${var.common.app_slug}_account_linked"
-  layers = [var.lambda_cognito_layer.arn]
+  layers           = var.lambda_cognito_layer.arn
   filename         = data.archive_file.account_linked.output_path
   source_code_hash = data.archive_file.account_linked.output_base64sha256
   role             = var.lambda_role.arn
@@ -14,9 +14,9 @@ resource "aws_lambda_function" "account_linked" {
   runtime          = "python3.12"
   timeout          = 60
   environment {
-    # variables = {
-    #   USER_POOL_ID = ""
-    #   }
+    variables = {
+      USER_POOL_ID = ""
+      }
   }
   depends_on = [data.archive_file.account_linked]
   tags       = local.common_tags
