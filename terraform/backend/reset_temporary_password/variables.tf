9706d41d501e758842a55a4c8820d0fd0d9dcd1d
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2024-12-02 14:08:49 -0500

    add env_vars to reset_temporary_password

--- terraform/backend/reset_temporary_password/variables.tf
+++ terraform/backend/reset_temporary_password/variables.tf
@@ -5,6 +5,7 @@ variable "rest_api" {}
 variable "authorizer_id" {}
 variable "lambda_role" {}
 variable "lambda_cognito_layer" {}
+variable "env_vars" {}
 
 locals {
   path_part = "reset_temporary_password"
