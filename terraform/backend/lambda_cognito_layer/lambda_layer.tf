2892a3d3e7d66611b1e5a1ec6a829fdaa88708b3
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2025-05-05 17:56:18 -0400

    fix triggers_replace when no dir exists

--- terraform/backend/lambda_cognito_layer/lambda_layer.tf
+++ terraform/backend/lambda_cognito_layer/lambda_layer.tf
@@ -1,48 +1,49 @@
 locals {
-  layer_name        = "lambda_cognito_layer"
-  description       = "Lambda layer that includes all the packages needed to link accounts in the web portal"
-  source_dir        = "backend\\lambda_cognito_layer\\src"
-  site_packages_dir = "${local.source_dir}\\python\\lib\\python3.13\\site-packages"
-  exclude_venv      = fileset("${path.module}/src/", ".venv/**/*")
-  exclude_pycache   = fileset("${path.module}/src/", "**/__pycache__/**/*")
-  exclude_dist_info = fileset("${path.module}/src/", "**/*.dist-info/**/*")
-  excludes = setunion(
-    local.exclude_venv,
-    local.exclude_pycache,
-    local.exclude_dist_info,
-    [
-      ".gitignore",
-    ]
-  )
+  layer_name      = "lambda_cognito_layer"
+  description     = "Lambda layer that includes all the packages needed to link accounts in the web portal"
+  runtime_name    = "python"
+  runtime_version = "3.13"
+  runtime         = "${local.runtime_name}${local.runtime_version}"
+  src_path        = "backend\\lambda_cognito_layer\\src"
+  packages_path   = "${local.src_path}\\python\\lib\\${local.runtime}\\site-packages"
+  last_rotation   = var.common.time.rotating.hours.12
+  mark_path       = "${local.packages_path}\\.mark"
 }
 
 resource "terraform_data" "pip_install" {
-  # Use this trigger if you need pip to run with EVERY terraform apply
-  # triggers_replace = timestamp()
-  # Use this trigger if you want pip to run only when requirements are changed 
-  #   note: Change some of the whitespace in requirements.txt to trigger one time
-  triggers_replace = filesha256("${path.module}/requirements.txt")
-
-  provisioner "local-exec" {
-    # This is so the archive_file will build on the first tf apply after cloning (source_dir doesn't exist yet)
-    command = "if not exist ${local.source_dir} mkdir ${local.source_dir} & echo I'm so dumb > .gitkeep"
+  triggers_replace = {
+    # Change whitespace in requirements.txt or delete site packages dir to trigger one time
+    requirements = filesha256("${path.module}/requirements.txt")
+    # If site packages does not exist, ensures it's built before trying to zip non-existent path
+    mark_file_exists = fileexists(local.mark_path)
+    # Ensures site packages are rebuilt and upgraded often
+    last_rotation = local.last_rotation
   }
 
   provisioner "local-exec" {
-    command = "if exist ${local.source_dir}\\python\\ rmdir ${local.source_dir}\\python /S /Q"
+    command = "if exist ${local.src_path}\\python\\ rmdir ${local.src_path}\\python /S /Q"
   }
 
   provisioner "local-exec" {
-    command = "pip install --platform manylinux2014_x86_64 --only-binary=:all: --no-binary=:none: --implementation cp --python-version 3.13 --upgrade -t ${local.site_packages_dir} -r ${path.module}\\requirements.txt"
+    command = "if not exist ${local.packages_path} mkdir ${local.packages_path} & echo ${timestamp()} > ${local.mark_path}"
+  }
+
+  provisioner "local-exec" {
+    command = "pip install --platform manylinux2014_x86_64 --only-binary=:all: --no-binary=:none: --implementation cp --python-version ${local.runtime_version} --upgrade -t ${local.packages_path} -r ${path.module}\\requirements.txt"
   }
 }
 
 data "archive_file" "deployment_package" {
   type        = "zip"
+  source_dir  = local.src_path
   output_path = "${path.module}/deployment/package.zip"
-  source_dir  = local.source_dir
-  excludes    = local.excludes
-  depends_on  = [terraform_data.pip_install]
+  excludes = setunion(
+    fileset("${path.module}/src/", ".venv/**/*"),
+    fileset("${path.module}/src/", "**/__pycache__/**/*"),
+    fileset("${path.module}/src/", "**/*.dist-info/**/*"),
+    fileset("${path.module}/src/", "**/.mark"),
+  )
+  depends_on = [terraform_data.pip_install]
 }
 
 resource "aws_s3_object" "deployment_package" {
@@ -64,6 +65,6 @@ resource "aws_lambda_layer_version" "lambda_cognito_layer" {
   s3_bucket                = aws_s3_object.deployment_package.bucket
   s3_key                   = aws_s3_object.deployment_package.key
   s3_object_version        = aws_s3_object.deployment_package.version_id
-  compatible_runtimes      = ["python3.13"]
+  compatible_runtimes      = [local.runtime]
   compatible_architectures = ["x86_64"]
 }
