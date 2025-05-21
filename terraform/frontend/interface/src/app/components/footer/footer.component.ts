03d7eda29401d48b6252c45ad534ca046a1af67f
Nathaniel Martin <nathaniel.martin.ctr@dot.gov>
2025-05-21 10:51:45 -0400

    replace other fhwa links with dot site

--- terraform/frontend/interface/src/app/components/footer/footer.component.ts
+++ terraform/frontend/interface/src/app/components/footer/footer.component.ts
@@ -13,13 +13,13 @@ export class FooterComponent implements OnInit {
   public phone: string = "202-366-4000";
 
   public footer_links: Array<any> = [
-    { title: 'Privacy Policy', url: 'https://www.fhwa.dot.gov/privacy.cfm' },
-    { title: 'Freedom of Information Act (FOIA)', url: 'https://www.fhwa.dot.gov/foia/' },
-    { title: 'Accessibility', url: 'https://www.fhwa.dot.gov/accessibility/' },
-    { title: 'Web Policies & Notices', url: 'https://www.fhwa.dot.gov/webpolicies/publishschedule.cfm' },
+    { title: 'Privacy Policy', url: 'https://www.transportation.gov/dot-website-privacy-policy' },
+    { title: 'Freedom of Information Act (FOIA)', url: 'https://www.transportation.gov/foia' },
+    { title: 'Accessibility', url: 'https://www.transportation.gov/mission/office-secretary/office-chief-information-officer/digital-accessibility-statement' },
+    { title: 'Web Policies & Notices', url: 'https://www.transportation.gov/web-policies' },
     { title: 'No Fear Act', url: 'https://www.transportation.gov/civil-rights/civil-rights-awareness-enforcement/no-fear-act' },
     { title: 'Report Waste, Fraud and Abuse', url: 'https://www.oig.dot.gov/Hotline' },
-    { title: 'U.S. DOT Home', url: 'https://www.transportation.gov/' },
+    // { title: 'U.S. DOT Home', url: 'https://www.transportation.gov/' },
     { title: 'USA.gov', url: 'https://www.usa.gov/' },
     { title: 'WhiteHouse.gov', url: 'https://www.whitehouse.gov/' }
   ];
