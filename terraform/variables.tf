5c0e0d8a4e8e4b5efc2dbcba573ecf8c51f330e5
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2026-07-27 16:22:17 -0400

    move client_id and client_secret to secret-manager

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,8 +15,6 @@ locals {
     disk_alert_linux_script     = data.terraform_remote_state.infrastructure.outputs.disk_alert_linux_script
     certificates                = data.terraform_remote_state.infrastructure.outputs.certificates
     app_slug                    = "portal2"
-    secrets_path                = "../../portal2-secrets"
-    client_secret               = file("../../portal2-secrets/backend/cognito/client_secret_${nonsensitive(data.aws_ssm_parameter.environment.value)}.txt")
     config_version              = var.config_version
     git_commit_head_sha1        = data.git_commit.head.sha1
     time = {
