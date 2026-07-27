d5359257160a48ba6d71290f538489fa8d7bfc14
Jeff Ussing <Jeff.Ussing.ctr@dot.gov>
2026-07-27 16:23:05 -0400

    bump build datetime

--- terraform/frontend/interface/src/environments/environment.prod.ts
+++ terraform/frontend/interface/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   stage: 'prod',
   build: '2.2.2',
-  buildDateTime: '2026-07-23 13:00 EST',
+  buildDateTime: '2026-07-28 17:30 EST',
   resource_urls: {
     portal: 'portal.sdc.dot.gov',
     portal_api: 'portal-api.sdc.dot.gov/v1',
